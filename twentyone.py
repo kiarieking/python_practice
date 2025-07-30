88176860e473f1309cd94c23cc5d9f2c586b1483
kiarieking <kiariekevin22@gmail.com>
2025-07-30 20:19:00 +0300

    started twentyone.py game

---
+++ twentyone.py
@@ -0,0 +1,17 @@
+print("Welcome to 21 number game!! The goal is to avoid choosing 21 on your turn.")
+import random
+x =[]
+turn_choice = input("Do you want to start first or second? Enter F for FIRST and S for SECOND : ")
+
+if turn_choice == "f" :
+    user_choice = input("Enter your number : ")
+    if user_choice < 21 and user_choice > 0:
+        x.append(user_choice)
+    else :
+        print("Invalid choice")
+
+elif turn_choice == "s" :
+    computer_choice = random.randint(0,22)
+    x.append(computer_choice)
+
+print(x)
